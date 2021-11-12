e0306648695c19d80709a1cf80fe6fb9f18155b3
Jay Shi <shijay@microsoft.com>
2021-11-11 19:13:22 -0800

    JayCSDK initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.jay.cs.basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
